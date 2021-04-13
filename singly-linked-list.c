4371ef05e7fb6c93453b24edfb1cee5f2e7a6c32
SHIN-sty <80668309+SHIN-sty@users.noreply.github.com>
2021-04-13 14:00:44 +0900

    Add files via upload

---
+++ singly-linked-list.c
@@ -0,0 +1,291 @@
+/*
+ * singly linked list
+ *
+ *  Data Structures
+ *  Department of Computer Science 
+ *  at Chungbuk National University
+ */
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+/* 필요한 헤더파일 추가 */
+
+typedef struct Node {
+	int key;
+	int data[4];
+	struct Node* link;
+} listNode;
+
+typedef struct Head {
+	struct Node* first;
+}headNode;
+
+
+/* 함수 리스트 */
+headNode* initialize(headNode* h);
+int freeList(headNode* h);
+
+int insertFirst(headNode* h, int key);
+int insertNode(headNode* h, int key);
+int insertLast(headNode* h, int key);
+
+int deleteFirst(headNode* h);
+int deleteNode(headNode* h, int key);
+int deleteLast(headNode* h);
+int invertList(headNode* h);
+
+void printList(headNode* h);
+
+int main()
+{
+	char command;
+	int key;
+	headNode* headnode=NULL;
+
+	do{
+		printf("----------------------------------------------------------------\n");
+		printf("                     Singly Linked List                         \n");
+		printf("----------------------------------------------------------------\n");
+		printf(" Initialize    = z           Print         = p \n");
+		printf(" Insert Node   = i           Delete Node   = d \n");
+		printf(" Insert Last   = n           Delete Last   = e\n");
+		printf(" Insert First  = f           Delete First  = t\n");
+		printf(" Invert List   = r           Quit          = q\n");
+		printf("----------------------------------------------------------------\n");
+
+		printf("Command = ");
+		scanf(" %c", &command);
+
+		switch(command) {
+		case 'z': case 'Z':
+			headnode = initialize(headnode);
+			break;
+		case 'p': case 'P':
+			printList(headnode);
+			break;
+		case 'i': case 'I':
+			printf("Your Key = ");
+			scanf("%d", &key);
+			insertNode(headnode, key);
+			break;
+		case 'd': case 'D':
+			printf("Your Key = ");
+			scanf("%d", &key);
+			deleteNode(headnode, key);
+			break;
+		case 'n': case 'N':
+			printf("Your Key = ");
+			scanf("%d", &key);
+			insertLast(headnode, key);
+			break;
+		case 'e': case 'E':
+			deleteLast(headnode);
+			break;
+		case 'f': case 'F':
+			printf("Your Key = ");
+			scanf("%d", &key);
+			insertFirst(headnode, key);
+			break;
+		case 't': case 'T':
+			deleteFirst(headnode);
+			break;
+		case 'r': case 'R':
+			invertList(headnode);
+			break;
+		case 'q': case 'Q':
+			freeList(headnode);
+			break;
+		default:
+			printf("\n       >>>>>   Concentration!!   <<<<<     \n");
+			break;
+		}
+
+	}while(command != 'q' && command != 'Q');
+
+	return 1;
+}
+
+headNode* initialize(headNode* h) {
+
+	/* headNode가 NULL이 아니면, freeNode를 호출하여 할당된 메모리 모두 해제 */
+	if(h != NULL)
+		freeList(h);
+
+	/* headNode에 대한 메모리를 할당하여 리턴 */
+	headNode* temp = (headNode*)malloc(sizeof(headNode));
+	temp->first = NULL;
+	return temp;
+}
+
+int freeList(headNode* h){
+	/* h와 연결된 listNode 메모리 해제
+	 * headNode도 해제되어야 함.
+	 */
+	listNode* p = h->first;
+
+	listNode* prev = NULL;
+	while(p != NULL) {
+		prev = p;
+		p = p->link;
+		free(prev);
+	}
+	free(h);
+	return 0;
+}
+
+
+
+/**
+ * list 처음에 key에 대한 노드하나를 추가
+ */
+int insertFirst(headNode* h, int key) {
+
+	listNode* node = (listNode*)malloc(sizeof(listNode));
+	node->key = key;
+
+	node->link = h->first;
+	h->first = node;
+
+	return 0;
+}
+
+
+/* 리스트를 검색하여, 입력받은 key보다 큰값이 나오는 노드 바로 앞에 삽입 */
+int insertNode(headNode* h, int key) {
+    listNode* tempNode;
+    tempNode = h -> first;
+    
+    while(tempNode != NULL) {
+		if(strcmp(tempNode -> data, key) == 0) return tempNode;
+        else tempNode = tempNode -> link;
+    }
+	return tempNode;
+	return 0;
+}
+
+/**
+ * list에 key에 대한 노드하나를 추가
+ */
+int insertLast(headNode* h, int key) {
+    listNode* temp;
+    listNode* node = (listNode*)malloc(sizeof(listNode));
+
+	node->key = key;
+	node -> link = NULL;
+	if(h -> first == NULL){
+	h -> first = node;
+    return 0;
+	}
+    temp = h -> first;
+	while( temp -> link != NULL)
+	temp = temp -> link;
+	temp -> link = node;
+}
+
+
+/**
+ * list의 첫번째 노드 삭제
+ */
+int deleteFirst(headNode* h) {
+    free(h);
+ 
+	return 0;
+}
+
+
+/**
+ * list에서 key에 대한 노드 삭제
+ */
+int deleteNode(headNode* h, int key) {
+listNode* prevNode;
+if(h -> first == NULL) return 0;
+if(h -> first -> link == NULL) {
+	free(h -> first);
+	h -> first = NULL;
+    return 0;
+}
+else if(h == NULL) return 0;
+else{
+   prevNode = h -> first;
+   while(prevNode -> link != h){
+	     prevNode = prevNode -> link;
+   }
+   prevNode -> link = h -> link;
+   free(h);
+}
+
+}
+
+/**
+ * list의 마지막 노드 삭제
+ */
+int deleteLast(headNode* h) {
+    listNode* prevNode;
+    listNode* delNode;
+	if(h -> first == NULL) return 0;
+	if(h -> first -> link == NULL) {
+		free(h -> first);
+		h -> first = NULL;
+    return 0;
+	}
+	 else {                //리스트에 노드가 2개 이상인 경우
+        prevNode = h -> first;
+        delNode = h -> first -> link;
+        while(delNode -> link != NULL) {
+            prevNode = delNode;
+            delNode = delNode -> link;
+        }
+        free(delNode);
+        prevNode -> link = NULL;
+    }
+}
+
+
+/**
+ * 리스트의 링크를 역순으로 재 배치
+ */
+int invertList(headNode* h) {
+    listNode* p;
+	listNode* q;
+	listNode* r;
+
+	p = h -> first;
+	q = NULL;
+	r = NULL;
+
+	while (p !=NULL){
+		r = q;
+		q = p;
+		p = p -> link;
+		q -> link = r;
+	}
+	h -> first = q;
+	return 0;
+}
+
+
+void printList(headNode* h) {
+	int i = 0;
+	listNode* p;
+
+	printf("\n---PRINT\n");
+
+	if(h == NULL) {
+		printf("Nothing to print....\n");
+		return;
+	}
+
+	p = h->first;
+
+	while(p != NULL) {
+		printf("[ [%d]=%d ] ", i, p->key);
+		p = p->link;
+		i++;
+	}
+
+	printf("  items = %d\n", i);
+}
+
